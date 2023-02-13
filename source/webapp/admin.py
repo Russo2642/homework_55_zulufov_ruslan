888577662aba3aacefaafb6ceb3d03526757366f
Ruslan Zulufov <gothiq2302@gmail.com>
2023-02-13 15:26:46 +0600

    Добавил модель и панель администратора

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from webapp.models import ToDo
+
+
 # Register your models here.
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'status', 'completion_at')
+    list_filter = ('id', 'description', 'status', 'completion_at')
+    search_fields = ('id', 'description')
+    fields = ('description', 'status', 'completion_at')
+    readonly_fields = ('id', 'completion_at')
+
+
+admin.site.register(ToDo, ToDoAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class ToDo(models.Model):
+    description = models.CharField(max_length=300, null=False, blank=False, help_text="Опишите задачу",
+                                   verbose_name="Описание")
+    status = models.CharField(max_length=200, null=False, blank=False, default="new", verbose_name="Статус")
+    completion_at = models.DateField(blank=True, verbose_name="Дата дедлайна")
+
+    def __str__(self):
+        return f"{self.status} - {self.description}"
